028a27095522e07d30348d42ed51e253814921ea
外马尔 <59741206+wangjian998@users.noreply.github.com>
2025-03-02 19:52:06 +0800

    Update diy-part2-gateway.sh

--- diy-part2-gateway.sh
+++ diy-part2-gateway.sh
@@ -182,9 +182,6 @@ config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client
 config_package_del luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server
 config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client
 config_package_del luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server
-## daed
-config_package_add daed
-
 # 添加 eBPF 支持
 config_add DEVEL
 config_add KERNEL_DEBUG_INFO
@@ -202,7 +199,8 @@ config_package_add kmod-sched-core
 config_package_add kmod-sched-bpf
 config_package_add kmod-xdp-sockets-diag
 config_package_add kmod-veth
-
+## daed
+config_package_add daed
 # 镜像生成
 # 修改分区大小
 sed -i "/CONFIG_TARGET_KERNEL_PARTSIZE/d" .config
